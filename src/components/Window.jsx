b0ac38afc99abffa6ad531d2fbb14909cdb8aad7
Andrew <abauer@chapman.edu>
2025-05-24 14:43:34 -0700

    Add window resizing

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -4,22 +4,126 @@ import closeX from "../images/window_controls/closeX.png";
 
 const Window = ({ isOpen, title, onClose }) => {
   const [position, setPosition] = useState({ x: 200, y: 100 });
-  const [isDragging, setIsDragging] = useState(false);
-  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+  const [isDraggingBar, setIsDraggingBar] = useState(false);
+  const [dragBarOffset, setDragBarOffset] = useState({ x: 0, y: 0 });
+  const [isResizing, setIsResizing] = useState(false);
+  const [resizingOffset, setResizingOffset] = useState({ x: 0, y: 0 });
   const [isHovered, setIsHovered] = useState(false);
   const [windowSize, setWindowSize] = useState({ width: 1000, height: 500 });
   const windowRef = useRef(null);
+  const [activeSizer, setActiveSizer] = useState(null);
 
   if (!isOpen) return null;
 
+  //-------------------------------- HANDLE RESIZE FUNCTIONALITY HERE ----------------------------------------------------
+  const handleResize = (e) => {
+    setIsResizing(true);
+    const rect = windowRef.current.getBoundingClientRect();
+    const sizerType = e.target.className
+      .split(" ")
+      .find((className) => className.includes("Sizer"))
+      .replace("Sizer", "");
+    setActiveSizer(sizerType);
+    setResizingOffset({
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    });
+  };
+
+  React.useEffect(() => {
+    if (isResizing) {
+      window.addEventListener("mousemove", resizeMove);
+      window.addEventListener("mouseup", resizeUnclick);
+    }
+    return () => {
+      window.removeEventListener("mousemove", resizeMove);
+      window.removeEventListener("mouseup", resizeUnclick);
+    };
+  }, [isResizing]);
+
+  const resizeMove = (e) => {
+    if (isResizing) {
+      const MIN_WIDTH = 300;
+      const MIN_HEIGHT = 200;
+      const MAX_WIDTH = window.innerWidth * 0.95;
+      const MAX_HEIGHT = window.innerHeight * 0.8;
+      let newY = position.y;
+      let newX = position.x;
+      let newWidth = windowSize.width;
+      let newHeight = windowSize.height;
+
+      if (activeSizer === "left") {
+        newWidth = windowSize.width + position.x - e.clientX;
+        newX = e.clientX;
+      } else if (activeSizer === "right") {
+        newWidth = e.clientX - position.x;
+      } else if (activeSizer === "top") {
+        newHeight = windowSize.height + position.y - e.clientY;
+        newY = e.clientY;
+      } else if (activeSizer === "bottom") {
+        newHeight = e.clientY - position.y;
+      } else if (activeSizer === "corner") {
+        newWidth = e.clientX - position.x;
+        newHeight = e.clientY - position.y;
+      }
+
+      //Set window constraints
+      if (newWidth < MIN_WIDTH) {
+        newWidth = MIN_WIDTH;
+        newX = position.x;
+      } else if (newWidth > MAX_WIDTH) {
+        newWidth = MAX_WIDTH;
+        newX = position.x;
+      }
+      if (newHeight < MIN_HEIGHT) {
+        newHeight = MIN_HEIGHT;
+        newY = position.y;
+      } else if (newHeight > MAX_HEIGHT) {
+        newHeight = MAX_HEIGHT;
+        newY = position.y;
+      }
+
+      //Can't size outside of website
+      if (newY < 25) {
+        newY = 25;
+        newHeight = windowSize.height + (position.y - 25);
+      } else if (newY + newHeight > window.innerHeight) {
+        // Prevent window from going below the viewport
+        newHeight = window.innerHeight - newY;
+      }
+
+      if (newX < 0) {
+        newX = 0;
+        newWidth = windowSize.width + position.x;
+      } else if (newX + newWidth > window.innerWidth) {
+        // Prevent window from going beyond right edge
+        newWidth = window.innerWidth - newX;
+      }
+
+      setWindowSize({
+        width: newWidth,
+        height: newHeight,
+      });
+      if (newX !== position.x || newY !== position.y) {
+        setPosition({ x: newX, y: newY });
+      }
+    }
+  };
+
+  const resizeUnclick = () => {
+    setIsResizing(false);
+    setActiveSizer(null);
+  };
+
+  //-------------------------------- HANDLE BAR FUNCTIONALITY HERE ----------------------------------------------------
   const handleBarClick = (e) => {
     if (
       e.target.className.includes("title-bar") ||
       e.target.className.includes("title-text")
     ) {
-      setIsDragging(true);
+      setIsDraggingBar(true);
       const rect = windowRef.current.getBoundingClientRect();
-      setDragOffset({
+      setDragBarOffset({
         x: e.clientX - rect.left,
         y: e.clientY - rect.top,
       });
@@ -27,28 +131,35 @@ const Window = ({ isOpen, title, onClose }) => {
   };
 
   const handleBarMove = (e) => {
-    if (isDragging) {
-      const newY = e.clientY - dragOffset.y;
-      // Only allow dragging if the new Y position is at or below the navbar (25px)
-      if (newY >= 25) {
-        setPosition({
-          x: e.clientX - dragOffset.x,
-          y: newY,
-        });
-      }
+    if (isDraggingBar) {
+      const newY = e.clientY - dragBarOffset.y;
+      const newX = e.clientX - dragBarOffset.x;
+
+      // Constrain Y position between navbar (25px) and window height
+      const constrainedY = Math.min(
+        Math.max(newY, 25),
+        window.innerHeight - windowSize.height
+      );
+
+      // Constrain X position between 0 and window width
+      const constrainedX = Math.min(
+        Math.max(newX, 0),
+        window.innerWidth - windowSize.width
+      );
+
+      setPosition({
+        x: constrainedX,
+        y: constrainedY,
+      });
     }
   };
 
   const handleBarUnclick = () => {
-    setIsDragging(false);
-  };
-
-  const closeWindow = () => {
-    onClose();
+    setIsDraggingBar(false);
   };
 
   React.useEffect(() => {
-    if (isDragging) {
+    if (isDraggingBar) {
       window.addEventListener("mousemove", handleBarMove);
       window.addEventListener("mouseup", handleBarUnclick);
     }
@@ -56,7 +167,12 @@ const Window = ({ isOpen, title, onClose }) => {
       window.removeEventListener("mousemove", handleBarMove);
       window.removeEventListener("mouseup", handleBarUnclick);
     };
-  }, [isDragging]);
+  }, [isDraggingBar]);
+
+  //CLOSE FUNCTIONALITY HERE
+  const closeWindow = () => {
+    onClose();
+  };
 
   return (
     <div
@@ -67,10 +183,13 @@ const Window = ({ isOpen, title, onClose }) => {
         top: position.y,
         width: windowSize.width,
         height: windowSize.height,
-        cursor: isDragging ? "grabbing" : "default",
+        cursor: isDraggingBar ? "grabbing" : "default",
       }}
     >
-      <section className="absolute topSizer w-full cursor-n-resize bg-transparent h-[2px] border-blue-500 border-2" />
+      <section
+        className="absolute topSizer w-full cursor-n-resize bg-transparent h-[2px] border-blue-500 border-2"
+        onMouseDown={handleResize}
+      />
       <div
         className="title-bar bg-gray-200 p-2 rounded-t-lg flex items-center h-9"
         onMouseDown={handleBarClick}
@@ -90,13 +209,25 @@ const Window = ({ isOpen, title, onClose }) => {
         </div>
       </div>
       <div className="flex-1 flex flex-col h-full">
-        <section className="absolute leftSizer border-purple-500 border-2 w-[2px] h-[calc(100%)] -translate-x-1 -translate-y-9 cursor-w-resize" />
+        <section
+          className="absolute leftSizer border-purple-500 border-2 w-[2px] h-[calc(100%)] -translate-x-1 -translate-y-9 cursor-w-resize"
+          onMouseDown={handleResize}
+        />
         <div className="p-4 w-full flex screen items-center justify-center h-[calc(100%-40px)] border-red-500 border-2">
           <p className="text-lg">hello world</p>
         </div>
-        <section className="absolute rightSizer right-0 border-orange-500 border-2 w-[2px] h-[calc(100%-8px)] cursor-e-resize -translate-y-9" />
-        <section className="absolute bottomSizer -bottom-1 border-green-500 border-2 h-2.5 w-[calc(100%-10px)] -translate-y-0.5 cursor-s-resize" />
-        <section className="absolute cornerSizer -bottom-2.5 right-0 border-yellow-500 border-2 h-[14px] w-[13px] -translate-y-1.5 translate-x-0.5 cursor-nwse-resize" />
+        <section
+          className="absolute rightSizer right-0 border-orange-500 border-2 w-[2px] h-[calc(100%-8px)] cursor-e-resize -translate-y-9"
+          onMouseDown={handleResize}
+        />
+        <section
+          className="absolute bottomSizer -bottom-1 border-green-500 border-2 h-2.5 w-[calc(100%-10px)] -translate-y-0.5 cursor-s-resize"
+          onMouseDown={handleResize}
+        />
+        <section
+          className="absolute cornerSizer -bottom-2.5 right-0 border-yellow-500 border-2 h-[14px] w-[13px] -translate-y-1.5 translate-x-0.5 cursor-nwse-resize"
+          onMouseDown={handleResize}
+        />
       </div>
     </div>
   );
