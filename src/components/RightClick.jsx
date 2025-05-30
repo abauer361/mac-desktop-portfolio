72e4f2c81ac5511976410788091323a52b8bb54c
Andrew <abauer@chapman.edu>
2025-05-29 20:18:35 -0700

    Add Right Click

--- src/components/RightClick.jsx
+++ src/components/RightClick.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const RightClick = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -40,19 +40,34 @@ const RightClick = () => {
 
   return (
     <div
-      className="fixed bg-white border border-gray-300 rounded shadow-lg py-2 z-50"
-      style={{
-        left: position.x,
-        top: position.y,
-      }}
+      className="fixed bg-white border border-gray-300 rounded shadow-lg py-2 z-50 select-none"
+      style={{ left: position.x, top: position.y }}
       onClick={handleMenuClick}
     >
-      <div className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        New Folder
+      <div
+        className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+        onClick={() => {
+          window.open("https://www.linkedin.com/in/andrew-m-bauer/", "_blank");
+        }}
+      >
+        My LinkedIn
       </div>
-      <div className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Refresh</div>
-      <div className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        Properties
+      <div
+        className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+        onClick={() => {
+          window.open(
+            "https://github.com/abauer361/mac-desktop-portfolio",
+            "_blank"
+          );
+        }}
+      >
+        Star This Project!
+      </div>
+      <div
+        className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+        onClick={() => window.open("mailto:andrewbauer361@gmail.com", "_blank")}
+      >
+        Contact Me
       </div>
     </div>
   );
