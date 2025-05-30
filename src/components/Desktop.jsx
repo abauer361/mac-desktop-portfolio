b3395321d2b0f7db765abcec4ba048754d1c69b4
Andrew <abauer@chapman.edu>
2025-05-29 20:19:34 -0700

    Remove border around desktop

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -7,7 +7,7 @@ import RightClick from "./RightClick";
 const Desktop = () => {
   return (
     <div
-      className="flex h-[calc(100%-90px)] flex-col justify-start content-start flex-wrap-reverse p-4 w-full gap-4 border"
+      className="flex h-[calc(100%-90px)] flex-col justify-start content-start flex-wrap-reverse p-4 w-full gap-4"
       id="desktop-container"
     >
       {/* This is where we map all our apps */}
